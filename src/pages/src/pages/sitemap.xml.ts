93c1de95c7ff14ceedffc69bbd5df02fb6f6c9c2
Jaime Syjuco <Jaime.Syjuco@aroodai.com>
2026-09-04 16:09:33 +0800

    Add sitemap.xml.ts to generate XML sitemap

---
+++ src/pages/src/pages/sitemap.xml.ts
@@ -0,0 +1,20 @@
+export async function GET() {
+  const siteUrl = process.env.PUBLIC_SITE_URL || 'https://singaporemusiclessons.com';
+  
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${siteUrl}/</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  return new Response(xml, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
